066c6d4cc51be563578b201a5b1a4c44e29530ae
Burgentie <63803079+Burgentie@users.noreply.github.com>
2020-09-04 20:03:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ client.on('ready', () => {
     console.log("active")
     const guild = client.guilds.cache.get('743952264032419959')
 
-    const userCountChannel = guild.channels.cache.get('751499101090087103')
-    const userCount = guild.members.cache.filter(member => !member.user.bot).size
-    userCountChannel.setName(`USER COUNT: ${userCount}`)
-
     const memberCountChannel = guild.channels.cache.get('751501642481926366')
     const memberCount = guild.members.cache.size
     memberCountChannel.setName(`MEMBER COUNT: ${memberCount}`)
